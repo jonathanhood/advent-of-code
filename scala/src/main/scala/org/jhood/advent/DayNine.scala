9561bc9d674a21f53ccbe458c492a4ed2890c423
Jonathan Hood <me@jonathanhood.org>
2018-12-24 15:35:49 -0600

    Solve Day 9

--- scala/src/main/scala/org/jhood/advent/DayNine.scala
+++ scala/src/main/scala/org/jhood/advent/DayNine.scala
@@ -1,38 +1,84 @@
 package org.jhood.advent
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object DayNine {
+  class BufferNode(var next: BufferNode, var last: BufferNode, val value: Long)
+
+  class CircularBuffer {
+    private var current: BufferNode = {
+      val node = new BufferNode(null,null,0)
+      node.next = node
+      node.last = node
+      node
+    }
+
+    @tailrec
+    final def clockwise(x: Long): Unit =
+      if(x > 0) {
+        current = current.next
+        clockwise(x - 1)
+      }
+
+    final def counterClockwise(x: Long): Unit =
+      if(x > 0) {
+        current = current.last
+        counterClockwise(x - 1)
+      }
+
+    def get: Long =
+      current.value
+
+    def insertClockwise(v: Long): Unit = {
+      val newNode = new BufferNode(current.next,current, v)
+      current.next.last = newNode
+      current.next = newNode
+    }
+
+    def removeAndClockwise: Unit = {
+      current.next.last = current.last
+      current.last.next = current.next
+      current = current.next
+    }
+  }
+
+  def score(players: Int, lastMarble: Long): Long =
+    play(
+      1,
+      lastMarble,
+      0,
+      mutable.Buffer.fill(players)(0),
+      new CircularBuffer
+    )
+
   @tailrec
-  def play(next: Int, max: Int, current: Int, player: Int, scores: List[Int], marbles: List[Int]): Int = {
+  def play(next: Long, max: Long, player: Int, scores: mutable.Buffer[Long], buffer: CircularBuffer): Long = {
     if(next == max + 1) {
       scores.max
     } else if((next % 23) == 0) {
-      val reverseStart = Stream.continually(marbles.reverse).flatten.dropWhile(_ != current)
-      val streamStart = Stream.continually(marbles).flatten.dropWhile(_ != current)
-      val removed = reverseStart.drop(7).head
-      print(s"Score! ${marbles.mkString(" ")}")
+      buffer.counterClockwise(7)
+      val removed = buffer.get
       val score = next + removed
-      val updatedMarbles = marbles.filter(_ != removed)
-      val updatedCurrent = streamStart.dropWhile(_ != current).head
+      buffer.removeAndClockwise
+      scores.update(player,scores(player) + score)
       play(
         next + 1,
         max,
-        updatedCurrent,
-        (player + 1) % scores.length,
-        scores.updated(player,scores(player) + score),
-        updatedMarbles
-      )
-    } else {
-      val streamStart = Stream.continually(marbles).flatten.dropWhile(_ != current)
-      val updatedMarbles = (streamStart.take(1) #::: next #:: streamStart.drop(1)).take(marbles.length + 1).toList
-      play(
-        next + 1,
-        max,
-        next,
         (player + 1) % scores.length,
         scores,
-        updatedMarbles
+        buffer
+      )
+    } else {
+      buffer.clockwise(1)
+      buffer.insertClockwise(next)
+      buffer.clockwise(1)
+      play(
+        next + 1,
+        max,
+        (player + 1) % scores.length,
+        scores,
+        buffer
       )
     }
   }
--- scala/src/test/scala/com/jhood/advent/DayNineTest.scala
+++ scala/src/test/scala/com/jhood/advent/DayNineTest.scala
@@ -5,15 +5,45 @@ import org.scalatest.FunSuite
 
 class DayNineTest extends FunSuite {
   test("part 1 - solve the example") {
-    val result = DayNine.play(
-      1,
-      25,
-      0,
-      0,
-      List.fill(9)(0),
-      List(0)
-    )
+    val result = DayNine.score(5,25)
 
     assert(result == 32)
+  }
+
+  test("part 1 - solve example 2") {
+    val result = DayNine.score(10,1618)
+    assert(result == 8317)
+  }
+
+  test("part 1 - solve example 3") {
+    val result = DayNine.score(13,7999)
+    assert(result == 146373)
+  }
+
+  test("part 1 - solve example 4") {
+    val result = DayNine.score(17,1104)
+    assert(result == 2764)
+  }
+
+  test("part 1 - solve example 5") {
+    val result = DayNine.score(21,6111)
+    assert(result == 54718)
+  }
+
+  test("part 1 - solve example 6") {
+    val result = DayNine.score(30,5807)
+    assert(result == 37305)
+  }
+
+  test("part 1 - solve") {
+    val result = DayNine.score(410, 72059)
+
+    println(s"Day 9 Part 1 - $result")
+  }
+
+  test("part 2 - solve") {
+    val result = DayNine.score(410, 7205900)
+
+    println(s"Day 9 Part 2 - $result")
   }
 }
