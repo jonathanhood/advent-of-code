22cc175c811a0c61098e03b47afbc3a955dfd44b
Jonathan Hood <jhood06@gmail.com>
2019-12-02 10:46:15 -0600

    Solve day 2 part 1.

---
+++ 2019/python/aoc/intcode.py
@@ -0,0 +1,89 @@
+from typing import List
+
+
+class Opcodes:
+    HALT = 99
+    ADD = 1
+    MULTIPLY = 2
+
+    INSTRUCTION_WIDTH = 4
+
+
+class ProgramHalted(Exception):
+    def __init__(self, pc: int):
+        self.pc = pc
+
+
+class InvalidOpcode(Exception):
+    def __init__(self, opcode: int, pc: int):
+        self.opcode = opcode
+        self.pc = pc
+
+
+def process_opcode(pc: int, memory: List[int]) -> None:
+    code = memory[pc]
+    if code == Opcodes.HALT:
+        raise ProgramHalted(pc)
+    elif code == Opcodes.ADD:
+        left_pointer = memory[pc + 1]
+        right_pointer = memory[pc + 2]
+        dest_pointer = memory[pc + 3]
+        result = memory[left_pointer] + memory[right_pointer]
+        store_result(memory, dest_pointer, result)
+    elif code == Opcodes.MULTIPLY:
+        left_pointer = memory[pc + 1]
+        right_pointer = memory[pc + 2]
+        dest_pointer = memory[pc + 3]
+        result = memory[left_pointer] * memory[right_pointer]
+        store_result(memory, dest_pointer, result)
+    else:
+        raise InvalidOpcode(code, pc)
+
+
+def store_result(memory: List[int], address: int,  value: int) -> None:
+    if address >= len(memory):
+        memory.extend([0] * (address - len(memory) + 1))
+    memory[address] = value
+
+
+def run_program(memory: List[int]) -> List[int]:
+    try:
+        pc = 0
+        while True:
+            process_opcode(pc, memory)
+            pc = pc + Opcodes.INSTRUCTION_WIDTH
+    except ProgramHalted:
+        return memory
+
+
+def load_program(program: str) -> List[int]:
+    return [int(op) for op in program.split(",")]
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Execute an intcode program.")
+    parser.add_argument("--replace",
+                        "-r",
+                        help="Replace a given memory position with the given value (e.g. '0,100')",
+                        action="append")
+    parser.add_argument("--peek",
+                        "-p",
+                        type=int,
+                        help="Peek at the given address after program execution finished",
+                        action="append")
+    parser.add_argument("program",
+                        help="The intcode program file.")
+
+    args = parser.parse_args()
+
+    with open(args.program) as program_file:
+        memory = load_program(program_file.read())
+        for replacement in args.replace:
+            (pos, value) = replacement.split(",")
+            store_result(memory, int(pos), int(value))
+        run_program(memory)
+        for peek_pos in args.peek:
+            print(f"{peek_pos} -> {memory[peek_pos]}")
+
---
+++ 2019/python/tests/test_intcode.py
@@ -0,0 +1,12 @@
+from aoc import intcode
+
+
+def test_small_programs():
+    assert intcode.run_program([1, 0, 0, 0, 99]) == [2, 0, 0, 0, 99]
+    assert intcode.run_program([2, 3, 0, 3, 99]) == [2, 3, 0, 6, 99]
+    assert intcode.run_program([2, 4, 4, 5, 99]) == [2, 4, 4, 5, 99, 9801]
+    assert intcode.run_program([1, 1, 1, 4, 99, 5, 6, 0, 99]) == [30, 1, 1, 4, 2, 5, 6, 0, 99]
+
+
+def test_load_program():
+    assert intcode.load_program("1, 0, 0, 0, 99") == [1, 0, 0, 0, 99]
